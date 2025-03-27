fd2a2bcff7f43a5caa6c679de9a126a2d5b9c5e8
Tim <timon-69@tut.by>
2025-03-27 17:04:18 +0100

    dfgdf

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,12 +1,20 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MainTest {
     @Test
     public static void testTitle() {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");  // Обязательно в CI
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.get("http://localhost:8080/");
         Assert.assertEquals(driver.getTitle(), "Sign in [Jenkins]");
         driver.quit();
